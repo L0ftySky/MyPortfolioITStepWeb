5e77e54cb1f98ed97634b12445664413b13517ac
группы стационара <st@itstep.lan>
2023-11-26 19:27:55 +0300

    Добавление исходного кода проекта

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+let menuToggler = document.querySelector(".menuToggler");
+let menu = document.querySelector(".menu");
+
+if(menuToggler) {
+    menuToggler.addEventListener("click", (e) => {
+        if(menuToggler.classList.contains("pushed")) {
+            menuToggler.classList.remove("pushed");
+            menu.classList.remove("opened");
+        } else{
+            menuToggler.classList.add("pushed");
+            menu.classList.add("opened")
+        }
+    });
+}
